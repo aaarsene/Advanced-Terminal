59de0dbf5f06f0b4b43304add8bbc784b611dfb0
druz320 <druz320@gmail.com>
2014-11-03 19:06:26 +0100

    string to int (colors)

--- terminal.h
+++ terminal.h
@@ -24,40 +24,41 @@ int kbhit(void)
 #define clrscr() printf("\033[2J")
 
 //Attributs
-#define RESET "0" //Réinitialise tous les attributs
-#define BOLD "1" //Haute intensité
-#define FAINT "2" //Basse intensité
-#define NORMAL "22" //Intensité normale
-#define UNDERSCORE "4" //Souligné
-#define UNDERSCORE_OFF "24" //Non souligné
-#define NEGATIVE "7" //Inverse la couleur du texte et du fond
-#define POSITIVE "27" //Remet la couleur du texte et du fond normalement
+#define RESET 0 //Réinitialise tous les attributs
+#define BOLD 1 //Haute intensité
+#define FAINT 2 //Basse intensité
+#define NORMAL 22 //Intensité normale
+#define UNDERSCORE 4 //Souligné
+#define UNDERSCORE_OFF 24 //Non souligné
+#define NEGATIVE 7 //Inverse la couleur du texte et du fond
+#define POSITIVE 27 //Remet la couleur du texte et du fond normalement
 
 //Couleurs  du texte
-#define BLACK "30"
-#define RED "31"
-#define GREEN "32"
-#define YELLOW "33"
-#define BLUE "34"
-#define MAGENTA "35"
-#define CYAN "36"
-#define WHITE "37"
-#define DEFAULT "39"
+#define BLACK 30
+#define RED 31
+#define GREEN 32
+#define YELLOW 33
+#define BLUE 34
+#define MAGENTA 35
+#define CYAN 36
+#define WHITE 37
+#define DEFAULT 39
 
 //Couleur du fond
-#define BG_BLACK "40"
-#define BG_RED "41"
-#define BG_GREEN "42"
-#define BG_YELLOW "43"
-#define BG_BLUE "44"
-#define BG_MAGENTA "45"
-#define BG_CYAN "46"
-#define BG_WHITE "47"
-#define BG_DEFAULT "49"
+#define BG_BLACK 40
+#define BG_RED 41
+#define BG_GREEN 42
+#define BG_YELLOW 43
+#define BG_BLUE 44
+#define BG_MAGENTA 45
+#define BG_CYAN 46
+#define BG_WHITE 47
+#define BG_DEFAULT 49
 
 // Sélection des attributs
-#define textattr(param) printf("\033[%sm",param)
+#define textattr(param) printf("\033[%dm",param)
 
+#define textattrV(param) printf("\033[%dm",param)
 // Place le curseur à la position x, y
 #define gotoxy(x, y) printf("\033[%d;%dH", y+1,x+1)
 
